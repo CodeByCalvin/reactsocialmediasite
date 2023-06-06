262a25f329a902a88ab401fac5972771fe0fc4b5
Calvin Merry <calvin@calvinmerry.com>
2023-06-06 15:47:59 +0100

    Starting code

---
+++ socialmediasite/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import UserPost from "./components/userpost";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>User posts</h1>
+      <UserPost />
+    </div>
+  );
+}
+
+export default App;
